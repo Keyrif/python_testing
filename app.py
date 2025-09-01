a977ce4623933cd29bffc9eb7181e9c83daaa58d
KEYRIF <mihaikenj@gmail.com>
2025-09-01 03:32:10 +0300

    yo

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# dicționar pentru comanda
-lista = {}
-
 @app.route("/")
 def home():
-    return render_template("menu.html", lista=lista)
-
-@app.route("/adauga/<produs>")
-def adauga(produs):
-    lista[produs] = lista.get(produs, 0) + 1
-    return redirect(url_for("home"))
-
-@app.route("/anulare")
-def anulare():
-    lista.clear()
-    return redirect(url_for("home"))
+    return render_template("menu.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
